170152c24b2bf81a70a6570d04db01333eb64979
kaede <kaede@frl.mech.tohoku.ac.jp>
2022-09-13 08:32:19 +0900

    ちょっとだけテストを足す

--- src/ik.rs
+++ src/ik.rs
@@ -22,27 +22,30 @@ impl<T: na::RealField> IK4dTriangle<T> {
             vc.x.clone() * vc.x.clone() + vc.y.clone() * vc.y.clone() + vc.z.clone() * vc.z.clone();
         let c = c_squared.sqrt();
         // cosine formula
-        ans[3] = ((self.a.clone() * self.a.clone() + self.b.clone() * self.b.clone() - c.clone() * c.clone())
+        ans[3] = ((self.a.clone() * self.a.clone() + self.b.clone() * self.b.clone()
+            - c.clone() * c.clone())
             / (T::from_f32(2.0).unwrap() * self.a.clone() * self.b.clone()))
         .acos();
 
-        let b_theta = ((self.a.clone() * self.a.clone() + c.clone() * c.clone() - self.b.clone()*self.b.clone())
-        / (T::from_f32(2.0).unwrap() * self.a.clone() * c.clone())).acos();
-        // let angle_cz = (vc.z.clone() / c.clone()).acos();   // angle between vc and z axis
+        // angle between vc and va
+        let b_theta = ((self.a.clone() * self.a.clone() + c.clone() * c.clone()
+            - self.b.clone() * self.b.clone())
+            / (T::from_f32(2.0).unwrap() * self.a.clone() * c.clone()))
+        .acos();
 
         // rotation from axis vertial to z axis and vc
-        let rot_cz = na::UnitQuaternion::from_axis_angle(&na::Unit::new_normalize(vc.cross(&na::Vector3::z_axis())), b_theta.clone());
+        let rot_cz = na::UnitQuaternion::from_axis_angle(
+            &na::Unit::new_normalize(vc.cross(&na::Vector3::z_axis())),
+            b_theta.clone(),
+        );
         let va_dot = rot_cz.transform_vector(&vc) * self.a.clone() / c.clone();
         let vb_dot = vc.clone() - va_dot.clone();
-        
-        // let r_z = self.a.clone() * (angle_cz.clone() - b_theta.clone()).cos();
-        // let ratio = (self.a.clone() *(angle_cz.clone() - b_theta.clone()).sin()) /(c.clone() * angle_cz.sin());
-        // let r_x = vc.x.clone() * ratio.clone();
-        // let r_y = vc.y.clone() * ratio;
-        // let r = na::Vector3::<T>::new(r_x, r_y, r_z);
 
         // rotation along with vector c
-        let rot_ref_theta = na::UnitQuaternion::from_axis_angle(&na::Unit::new_normalize(vc.clone()), self.ref_theta.clone());
+        let rot_ref_theta = na::UnitQuaternion::from_axis_angle(
+            &na::Unit::new_normalize(vc.clone()),
+            self.ref_theta.clone(),
+        );
         let va = rot_ref_theta.transform_vector(&va_dot);
         let vb = rot_ref_theta.transform_vector(&vb_dot);
         ans[0] = va.y.clone().atan2(va.x.clone());
@@ -55,11 +58,8 @@ impl<T: na::RealField> IK4dTriangle<T> {
         let vb_tmp = vb_dot.clone() - va_tmp.clone();
         let vb_tmp2 = vb.clone() - va_tmp.clone();
 
-        // let v1 = vc.clone() - r_dot.clone() * (self.a.clone() - self.b.clone() * b_theta.clone().cos()) / self.a.clone();
-        // let mut v2 = - r_dot.clone() * (self.a.clone() - self.b.clone() * b_theta.clone().cos()) / self.a.clone();
-        // v2.z += self.b.clone() * b_theta.clone().sin();
-
-        ans[2] = va.clone().dot(&vb_tmp.cross(&vb_tmp2)).signum() * (vb_tmp.dot(&vb_tmp2) / vb_tmp.dot(&vb_tmp)).acos();
+        ans[2] = va.clone().dot(&vb_tmp.cross(&vb_tmp2)).signum()
+            * (vb_tmp.dot(&vb_tmp2) / vb_tmp.dot(&vb_tmp)).acos();
 
         ans
         // 👺原点と方向の定義も必要
@@ -79,6 +79,18 @@ mod test_ik {
         ik.ref_theta = 0.0;
         let ans = ik.solve(&na::Vector3::new(3.0, 0.0, 0.0));
         assert_relative_eq!(ans[0], 0.0, epsilon = 1.0e-6);
+        assert_relative_eq!(ans[1], core::f32::consts::PI / 6.0, epsilon = 1.0e-6);
+        assert_relative_eq!(ans[2], 0.0, epsilon = 1.0e-6);
+        assert_relative_eq!(ans[3], core::f32::consts::PI / 3.0, epsilon = 1.0e-6);
+        ik.a = 3.0;
+        ik.b = 3.0;
+        ik.ref_theta = 0.0;
+        let ans = ik.solve(&na::Vector3::new(
+            3.0 / (2.0 as f32).sqrt(),
+            3.0 / (2.0 as f32).sqrt(),
+            0.0,
+        ));
+        assert_relative_eq!(ans[0], core::f32::consts::PI / 4.0, epsilon = 1.0e-6);
         assert_relative_eq!(ans[1], core::f32::consts::PI / 6.0, epsilon = 1.0e-6);
         assert_relative_eq!(ans[2], 0.0, epsilon = 1.0e-6);
         assert_relative_eq!(ans[3], core::f32::consts::PI / 3.0, epsilon = 1.0e-6);
